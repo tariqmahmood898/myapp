eb72d27311ed965d7e18207599ed5ad613e1c633
Tariq Mahmood <tariqmahmood898@gmail.com>
2025-10-16 23:20:31 +0500

    Update constants.ts

--- src/util/deeplink/constants.ts
+++ src/util/deeplink/constants.ts
@@ -4,11 +4,11 @@ export const TONCONNECT_PROTOCOL = 'tc://';
 export const TONCONNECT_PROTOCOL_SELF = 'mytonwallet-tc://';
 
 
-export const SELF_PROTOCOL = 'https://t.me/DPSwallet_bot/?startapp';
-
-
-export const SELF_UNIVERSAL_URLS = ['https://walletdps.netlify.app', 'https://t.me/DPSwallet_bot/?startapp'];
-
+export const SELF_PROTOCOL = 'https://t.me/DPSwallet_bot?startapp=';
+export const SELF_UNIVERSAL_URLS = [
+  'https://walletdps.netlify.app',
+  'https://t.me/DPSwallet_bot?startapp='
+];
 
 export const TONCONNECT_UNIVERSAL_URL = 'https://connect.mytonwallet.org';
 export const CHECKIN_URL = 'https://checkin.mytonwallet.org';
